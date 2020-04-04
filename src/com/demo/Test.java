20b290ec1d0155ba9df1ba91e50d693dceaeaf95
wonderful1234 <45567501+wonderful1234@users.noreply.github.com>
2020-04-04 11:21:20 +0800

    测试

--- src/com/demo/Test.java
+++ src/com/demo/Test.java
@@ -5,7 +5,7 @@ import com.aliyuncs.exceptions.ClientException;
 public class Test {
 	public static void main(String[] args) throws ClientException {
 		SendInformation r=new SendInformation();
-		String y=SendInformation.sendSms("17318702983");
+		String y=SendInformation.sendSms("xxxxxxxxxxx");
 		System.out.println(y);
 	}
 
